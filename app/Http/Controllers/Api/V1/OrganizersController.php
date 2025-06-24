eb8e5a1aa15b64f5db797201eaa00868d62926ee
Dynaudio <dynaudioputra25@gmail.com>
2025-06-25 05:04:07 +0800

    Organizers Controller Added

--- app/Http/Controllers/Api/V1/OrganizersController.php
+++ app/Http/Controllers/Api/V1/OrganizersController.php
@@ -8,7 +8,31 @@ use App\Http\Controllers\Controller;
 
 class OrganizersController extends Controller
 {
-    public function index()
+    public function index(Request $request)
+    {
+        $limit = $request->query('limit', 5);
+
+        $organizers = Organizers::with('images')->paginate((int) $limit);
+
+        if ($organizers->count() === 0) {
+            return response()->json([
+                'message' => 'Organizers Not Found'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $organizers->items(),
+            'message' => 'Event Organizers tersedia',
+            'meta' => [
+                'current_page' => $organizers->currentPage(),
+                'total' => $organizers->total(),
+                'per_page' => $organizers->perPage(),
+                'last_page' => $organizers->lastPage()
+            ]
+        ], 200);
+    }
+
+    public function getAll()
     {
         $organizers = Organizers::with('images')->get();
 
@@ -21,5 +45,36 @@ class OrganizersController extends Controller
         return response()->json([
             'data' => $organizers
         ], 200);
+    }
+
+    public function show($id)
+    {
+        $organizer = Organizers::with('images')->find($id);
+
+        if (!$$organizer) {
+            return response()->json([
+                'message' => 'Event Organizers Not Found'
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Event Organizers Tersedia',
+            'data' => $organizer
+        ], 200);
+    }
+
+    public function search($name): void {
+        $organizers = Organizers::with('images')->where('name', 'like', '%'.$name.'%')->orderBy('name')->get();
+    
+        if (count($name) > 0) {
+            return response()->json([
+                'data' => $organizers
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Event Organizers Not Found',
+                'data' => $organizers
+            ], 404);
+        }
     }
 }
